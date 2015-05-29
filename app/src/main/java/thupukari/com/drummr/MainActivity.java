1a6992d430e01f07892be4ca0397720e3dce964d
helloworld <sthupukari@gmail.com>
2015-05-29 06:41:18 -0400

    Initial Commit

---
+++ app/src/main/java/thupukari/com/drummr/MainActivity.java
@@ -0,0 +1,102 @@
+package thupukari.com.drummr;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+
+public class MainActivity extends ActionBarActivity {
+
+    private Socket socket;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            socket = IO.socket("http://104.236.192.49:3000/");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                Log.d("MainActivity: ", "socket connected");
+                socket.emit("add user", "device");
+            }
+
+        }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+            }
+
+        });
+
+        socket.connect();
+
+        setContentView(R.layout.activity_main);
+
+        Button buttons[][] = new Button[3][2];
+
+        for(int i = 0; i < buttons.length; i++){
+            for(int j = 0; j < buttons[i].length; j++ ){
+                String buttonID = "button" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = (Button) findViewById(resID);
+                buttons[i][j].setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View arg0, MotionEvent arg1) {
+                        if (arg1.getAction() == MotionEvent.ACTION_DOWN){
+                            socket.emit("button down", getResources().getResourceEntryName(arg0.getId()));
+                            return false;
+                        }
+                        if (arg1.getAction() == MotionEvent.ACTION_UP){
+                            socket.emit("button up", getResources().getResourceEntryName(arg0.getId()));
+                            return false;
+                        }
+                        return true;
+                    }
+                });
+            }
+        }
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
